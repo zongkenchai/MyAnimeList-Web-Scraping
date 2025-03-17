62b14fd0b308a9c6bde372465ce842675911a643
Chai Zong Ken <zongken.c@involve.asia>
2025-03-17 14:52:18 +0800

    ✨ Save as a feather format

--- get_myanimelist_data/main.py
+++ get_myanimelist_data/main.py
@@ -24,17 +24,9 @@ if __name__ == "__main__":
     #* Default values
     lower_rank = 0
     upper_rank = 50
-    root_dir = os.getcwd()
-    data_folder = os.path.join(root_dir, 'data')
-    raw_file_name = f'{int(time.time())}_{lower_rank}-{upper_rank}_raw.feather'
-    raw_save_path = os.path.join(data_folder, raw_file_name)
+    chunk_size = 50
 
-    final_file_name = f'{int(time.time())}_{lower_rank}-{upper_rank}_final.feather'
-    final_save_path = os.path.join(data_folder, final_file_name)
-
-
-    main_anime_df = pd.DataFrame()
-
+    
     if args.rank is not None:
         if '-' in args.rank:
             lower_rank, upper_rank = map(int, args.rank.split('-'))
@@ -42,10 +34,18 @@ if __name__ == "__main__":
             upper_rank = int(args.rank)
 
     
-    page_limit_list = [i for i in range(lower_rank,upper_rank,50)]
+    root_dir = os.getcwd()
+    data_folder = os.path.join(root_dir, 'data')
+    raw_file_name = f'{lower_rank}_{upper_rank}_raw.feather'
+    raw_save_path = os.path.join(data_folder, raw_file_name)
 
+    
+    page_limit_list = [i for i in range(lower_rank,upper_rank,50)]
+    logger.info(page_limit_list)
     web_driver = WebDriver()
     web_driver.start_driver()
+
+    main_anime_df = pd.DataFrame()
 
     for page_limit in page_limit_list:
         logger.info(f'Obtaining Anime Information from {page_limit}')
@@ -55,34 +55,41 @@ if __name__ == "__main__":
 
         main_anime_df = pd.concat([main_anime_df, filtered_anime_df])
 
-
     main_anime_df.to_feather(raw_save_path)
 
-    
+
+
     raw_df = pd.read_feather(raw_save_path)
     logger.info(raw_df)
-    main_df = pd.DataFrame()
     
-    for row in raw_df.iterrows():
-        row = row[1]
-        rank = row['rank']
-        link = row['link']
-        logger.info(f'Getting Detail for {rank}')
-
+    no_of_rows = raw_df.shape[0]
+    for start in range(0, no_of_rows, chunk_size):
+        main_detail_df = pd.DataFrame()
+        detail_file_name = f'{start}_{start+chunk_size}_final.feather'
+        detail_save_path = os.path.join(data_folder, detail_file_name)
         
-        detail_dict = AnimeDetail.obtain_anime_detail(
-            driver=web_driver.driver,
-            anime_link=link,
-            rank=rank
-        )
+        tmp_df = raw_df[start:start+chunk_size]
+    
+        for row in tmp_df.iterrows():
+            row = row[1]
+            rank = row['rank']
+            link = row['link']
+            logger.info(f'Getting Detail for {rank}')
 
-        for i in detail_dict:
-            row[i[0]] = i[1]
-        row = pd.DataFrame(row.to_dict(),index=[rank])
-        main_df = pd.concat([main_df,row])
-        time.sleep(10)
+            
+            detail_dict = AnimeDetail.obtain_anime_detail(
+                driver=web_driver.driver,
+                anime_link=link,
+                rank=rank
+            )
 
-    main_anime_df.to_feather(final_save_path)
+            for i in detail_dict:
+                row[i[0]] = i[1]
+            row = pd.DataFrame(row.to_dict(),index=[rank])
+            main_detail_df = pd.concat([main_detail_df,row])
+            time.sleep(10)
+
+        main_detail_df.to_feather(detail_save_path)
         
     web_driver.driver.quit()
     web_driver.driver = None
