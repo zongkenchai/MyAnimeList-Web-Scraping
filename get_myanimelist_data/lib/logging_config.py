cfc08d7d7b725adab11df3737221b0d0105cd253
Chai Zong Ken <zongken3018@gmail.com>
2025-03-19 22:13:56 +0800

    🔊 Update logger configs

--- get_myanimelist_data/lib/logging_config.py
+++ get_myanimelist_data/lib/logging_config.py
@@ -1,11 +1,15 @@
+from datetime import date
 import logging
 # from app import create_app
 
 logger = logging.getLogger(__name__)
 handler = logging.StreamHandler()
-formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+formatter = logging.Formatter(fmt='%(asctime)s - %(name)s - %(levelname)s - %(funcName)s - %(message)s', 
+                              datefmt="%Y-%m-%d %H:%M:%S"
+)
+
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.propagate = False
 
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
