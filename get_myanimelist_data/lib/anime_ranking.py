57aaa3d875a825d972de151a79744244bbd0b3bf
Chai Zong Ken <zongken3018@gmail.com>
2025-03-19 22:13:35 +0800

    ✨ Clean up the main command file, reformat to use polars instead of pandas

--- get_myanimelist_data/lib/anime_ranking.py
+++ get_myanimelist_data/lib/anime_ranking.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import polars as pl
 from selenium.webdriver.common.by import By
 from lib.logging_config import logger
 
@@ -10,7 +11,7 @@ class AnimeRanking:
         return f'{self.base_link}?limit={lower_bound}'
 
 
-    def obtain_data_from_page(self, driver, lower_bound:int) -> pd.DataFrame:
+    def obtain_data_from_page(self, driver, lower_bound:int) -> pl.DataFrame:
         link = self.get_link(lower_bound=lower_bound)
         
         driver.get(link)
@@ -18,9 +19,13 @@ class AnimeRanking:
         name_of_anime = [i.text for i in driver.find_elements(by=By.CSS_SELECTOR, value="tr.ranking-list div.detail .di-ib h3 a")]
         link_of_anime = [i.get_attribute("href") for i in driver.find_elements(by=By.CSS_SELECTOR, value="tr.ranking-list div.detail .di-ib h3 a")]
         scores = [float(i.text) for i in driver.find_elements(by=By.CSS_SELECTOR, value="tr.ranking-list td.score span.score-label")]
-        anime_df = pd.DataFrame(list(zip(rank_of_anime, name_of_anime, link_of_anime, scores)), columns=["rank", "name","link","score"])
+        anime_df = pl.DataFrame({
+            "rank": rank_of_anime,
+            "name": name_of_anime,
+            "link": link_of_anime,
+            "score": scores
+        })
 
-        # self.main_anime_df = pd.concat([self.main_anime_df,anime_df])
 
         return anime_df
 
--- get_myanimelist_data/main.py
+++ get_myanimelist_data/main.py
@@ -1,15 +1,18 @@
 import logging
-import pandas as pd
+import polars as pl
 import time
 import os
 from argparse import ArgumentParser
-
+from pathlib import Path
 from lib.anime_ranking import AnimeRanking
 from lib.anime_details import AnimeDetail
 from lib.web_driver import WebDriver
+from lib.logging_config import logger
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger('get_myanimelist_data')
+#* Default values
+lower_rank = 0
+upper_rank = 50
+chunk_size = 50
 
 if __name__ == "__main__":
     args_parser = ArgumentParser(
@@ -17,79 +20,105 @@ if __name__ == "__main__":
     
     args_parser.add_argument("--rank",
                              help="seperate the lower and upper bound by -, if only a number being given will be assumed as the upper bound",
-                             dest='rank', action='store')
-    
+                             dest='rank',
+                             action='store',
+                             required=True)
+    args_parser.add_argument("--mode",
+                             help="Whether to get the list of rank or not",
+                             dest='mode',
+                             choices=['all', 'rank', 'detail'],
+                             default='all',
+                             action='store',
+                             required=False)
+
+
     args = args_parser.parse_args()
-
-    #* Default values
-    lower_rank = 0
-    upper_rank = 50
-    chunk_size = 50
-
-    
     if args.rank is not None:
         if '-' in args.rank:
             lower_rank, upper_rank = map(int, args.rank.split('-'))
         else:
             upper_rank = int(args.rank)
 
-    
+    mode = args.mode
+
+
     root_dir = os.getcwd()
     data_folder = os.path.join(root_dir, 'data')
-    raw_file_name = f'{lower_rank}_{upper_rank}_raw.feather'
-    raw_save_path = os.path.join(data_folder, raw_file_name)
+    raw_folder = os.path.join(data_folder, 'raw')
+    output_folder = os.path.join(data_folder, 'output')
+
+    #* Create the folder if it does not exist
+    if not os.path.exists(data_folder):
+        os.makedirs(data_folder)
+    if not os.path.exists(raw_folder):
+        os.makedirs(raw_folder) 
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
 
     
     page_limit_list = [i for i in range(lower_rank,upper_rank,50)]
-    logger.info(page_limit_list)
+    logger.debug(f'Page Limit List: {page_limit_list}')
     web_driver = WebDriver()
     web_driver.start_driver()
 
-    main_anime_df = pd.DataFrame()
+    if mode == 'all' or mode == 'rank':
+        for page_limit in page_limit_list:
+            raw_file_name = f'{page_limit}_{page_limit+50}.feather'
+            raw_save_path = os.path.join(raw_folder, raw_file_name)
+            logger.debug(f'Raw Save Path: {raw_save_path}')
+            logger.info(f'Obtaining Anime Information from {page_limit}')
+            anime_ranking = AnimeRanking()
+            anime_df = anime_ranking.obtain_data_from_page(driver=web_driver.driver, lower_bound=page_limit)
+            filtered_anime_df = anime_df\
+                .filter((pl.col("rank") >= lower_rank) & (pl.col("rank") <= upper_rank))
 
-    for page_limit in page_limit_list:
-        logger.info(f'Obtaining Anime Information from {page_limit}')
-        anime_ranking = AnimeRanking()
-        anime_df = anime_ranking.obtain_data_from_page(driver=web_driver.driver, lower_bound=page_limit)
-        filtered_anime_df = anime_df[(anime_df['rank'] >= lower_rank) & (anime_df['rank'] <= upper_rank)]
+            filtered_anime_df.write_ipc(raw_save_path)
 
-        main_anime_df = pd.concat([main_anime_df, filtered_anime_df])
-
-    main_anime_df.to_feather(raw_save_path)
+        logger.info('Completed saving of the rank file')
 
 
+    if mode == 'all' or mode == 'detail':
+        logger.info('Getting Detail Information')
+        raw_df = pl.concat([pl.read_ipc(file) for file in Path(raw_folder).glob("*.feather")])
 
-    raw_df = pd.read_feather(raw_save_path)
-    logger.info(raw_df)
-    
-    no_of_rows = raw_df.shape[0]
-    for start in range(0, no_of_rows, chunk_size):
-        main_detail_df = pd.DataFrame()
-        detail_file_name = f'{start}_{start+chunk_size}_final.feather'
-        detail_save_path = os.path.join(data_folder, detail_file_name)
+        # Deduplicate, filter, and sort
+        raw_df = (
+            raw_df.unique(subset=["rank", "name"])
+            .filter((pl.col("rank") >= lower_rank) & (pl.col("rank") <= upper_rank))
+            .sort("rank")
+        )
+
+        logger.info(raw_df)
         
-        tmp_df = raw_df[start:start+chunk_size]
-    
-        for row in tmp_df.iterrows():
-            row = row[1]
-            rank = row['rank']
-            link = row['link']
-            logger.info(f'Getting Detail for {rank}')
+        # Get total rows
+        no_of_rows = raw_df.height  
 
-            
-            detail_dict = AnimeDetail.obtain_anime_detail(
-                driver=web_driver.driver,
-                anime_link=link,
-                rank=rank
-            )
+        for start in range(0, no_of_rows, chunk_size):
+            detail_file_name = f"{start}_{start + chunk_size}.feather"
+            detail_save_path = os.path.join(output_folder, detail_file_name)
 
-            for i in detail_dict:
-                row[i[0]] = i[1]
-            row = pd.DataFrame(row.to_dict(),index=[rank])
-            main_detail_df = pd.concat([main_detail_df,row])
-            time.sleep(10)
+            # Get chunk
+            tmp_df = raw_df.slice(start, chunk_size)  
 
-        main_detail_df.to_feather(detail_save_path)
+            details = []
+
+            for row in tmp_df.iter_rows(named=True):  # Iterate as dictionaries
+                rank = row["rank"]
+                link = row["link"]
+                logger.info(f"Getting Detail for {rank}")
+
+                detail_dict = AnimeDetail.obtain_anime_detail(
+                    driver=web_driver.driver, anime_link=link, rank=rank
+                )
+
+                # Merge detail_dict into row
+                row.update({k: v for k, v in detail_dict})
+
+                details.append(row)
+                time.sleep(10)
+
+            # Convert to Polars DataFrame and save
+            pl.DataFrame(details).write_ipc(detail_save_path)
         
     web_driver.driver.quit()
     web_driver.driver = None
